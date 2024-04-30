cab3abe4fe760054c8f18b86613e429276d98004
Natalia Fedyk <124808493+Perslay@users.noreply.github.com>
2024-04-30 23:08:32 +0200

    Refractor
    Using function instead of class. Using hook useState()

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,60 +1,58 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import css from '../styles/App.module.css';
 import { Statistics } from './Statistics';
 import { FeedbackOptions } from './FeedbackOptions';
 import { Notification } from './Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleClick = feedbackType => {
+    if (feedbackType === 'good') {
+      setGood(good + 1);
+    } else if (feedbackType === 'neutral') {
+      setNeutral(neutral + 1);
+    } else {
+      setBad(bad + 1);
+    }
   };
 
-  handleClick = feedbackType => {
-    this.setState(prevState => ({
-      [feedbackType]: prevState[feedbackType] + 1,
-    }));
-  };
-
-  countTotalFeedback = () => {
-    const total = this.state.good + this.state.neutral + this.state.bad;
+  const countTotalFeedback = () => {
+    const total = good + neutral + bad;
     return total;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const percentage = (this.state.good * 100) / this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const percentage = (good * 100) / countTotalFeedback();
     return Math.round(percentage, 3);
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-
-    return (
-      <div className={css.appContainer}>
-        <div>
-          <p className={css.paragraph}>Please leave feedback</p>
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.handleClick}
-          />
-        </div>
-        <div>
-          <p className={css.paragraph}>Statistics</p>
-          <Notification
-            message="There is no feedback"
-            feedbackGiven={good > 0 || neutral > 0 || bad > 0}
-          >
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          </Notification>
-        </div>
+  return (
+    <div className={css.appContainer}>
+      <div>
+        <p className={css.paragraph}>Please leave feedback</p>
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleClick}
+        />
       </div>
-    );
-  }
-}
+      <div>
+        <p className={css.paragraph}>Statistics</p>
+        <Notification
+          message="There is no feedback"
+          feedbackGiven={good > 0 || neutral > 0 || bad > 0}
+        >
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        </Notification>
+      </div>
+    </div>
+  );
+};
